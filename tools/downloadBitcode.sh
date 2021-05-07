14af79ddddc272c320f136713e534b6816a1c546
JongWoo Park <jjongwoo.dev@gmail.com>
2021-05-07 14:44:13 +0900

    fix: bitcode 다운로드 주소 변경

--- tools/downloadBitcode.sh
+++ tools/downloadBitcode.sh
@@ -2,12 +2,18 @@
 
 set -euo pipefail
 
-# Files to be downloaded
-WEBRTC_BUILD="M87-1"
-WEBRTC_FILE="https://dl.bintray.com/webrtc-builds/webrtc-builds/${WEBRTC_BUILD}/WebRTC.tar.xz"
-
-
 THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
+
+# PACKAGE_VERSION=$(cat ${THIS_DIR}/../package.json \
+#   | grep "\"version\":" \
+#   | head -1 \
+#   | awk -F: '{ print $2 }' \
+#   | sed 's/[",]//g' \
+#   | tr -d '[[:space:]]')
+
+PACKAGE_VERSION="1.89.1" # 원래는 87버전이었는데, 해당 파일이 존재하지 않아 89로 업데이트했습니다.
+
+WEBRTC_DL="https://s3.ap-northeast-2.amazonaws.com/tenuto.dev/${PACKAGE_VERSION}/WebRTC.tar.xz"
 
 pushd ${THIS_DIR}/../apple
 
@@ -16,7 +22,8 @@ rm -rf WebRTC.xcframework WebRTC.dSYMs
 
 # Download
 echo "Downloading files..."
-curl -L -s ${WEBRTC_FILE} | tar Jxf -
+echo $PACKAGE_VERSION
+curl -L ${WEBRTC_DL} | tar Jxf -
 echo "Done!"
 
 popd
